85e45de3a5f6e09aaa3a6cf2fe81877dc0c2fee8
XMNXofficial <mchaohao159@outlook.com>
2023-10-17 01:04:38 +0800

    1

--- MusicPlayer.cpp
+++ MusicPlayer.cpp
@@ -1,12 +1,25 @@
-﻿// MusicPlayer.cpp: 定义应用程序的入口点。
-//
-
-#include "MusicPlayer.h"
-
+﻿#include "MusicPlayer.hpp"
+#include <hello_imgui.h>
 using namespace std;
+void showgui();
+ImFont* gFont = nullptr;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	HelloImGui::RunnerParams p;
+	p.appWindowParams.windowGeometry.size = { 800, 600 };
+	p.imGuiWindowParams.defaultImGuiWindowType = HelloImGui::DefaultImGuiWindowType::ProvideFullScreenWindow;
+	p.callbacks.ShowGui = showgui;
+	p.callbacks.LoadAdditionalFonts = []() {
+		ImFontGlyphRangesBuilder a;
+		static ImVector<ImWchar>b;
+		a.AddRanges(ImGui::GetIO().Fonts->GetGlyphRangesChineseFull());
+		a.BuildRanges(&b);
+		gFont = ImGui::GetIO().Fonts->AddFontFromFileTTF("C:\\Windows\\Fonts\\msyhbd.ttc", 20.0f, nullptr, b.Data); };
+	HelloImGui::Run(p);
 	return 0;
+}
+void showgui()
+{
+
 }
